bc6a3b733d7753cf395a317880695c23fdb82cfb
Devin R <devinr@moback.com>
2015-07-17 14:31:54 -0700

    some exception handling, of sorts

--- source/app.d
+++ source/app.d
@@ -17,6 +17,8 @@ termios shell_tmodes;
 int shell_terminal;
 int shell_is_interactive;
 
+bool sigintCalled = false;
+
 void main()
 {
     writeln("Starting Shell...");
@@ -25,22 +27,49 @@ void main()
     char[] line;
     while(true)
     {
-        if(readln(line) && line !is null) {
-            if(line.length > 0)
+        try
+        {
+            if(readln(line) && line !is null)
             {
-                line = chomp(line);
-                if(line == "exit")
+                if(line.length > 0)
                 {
-                    exit(0);
+                    line = chomp(line);
+                    if(line == "exit")
+                    {
+                        exit(0);
+                    }
+                    writeln(line);
                 }
-                writeln(line);
             }
-        } else {
-            //When ctrl+d is pressed and it's the only thing on the line,
-            //line is set to null. We could try to continue taking input and ignore
-            //the ctrl+d press, but exiting seems a bit more standard...
-            writeln("Terminating due to end of input.");
-            exit(0);
+            else
+            {
+                /*
+                 * When ctrl+d is pressed and it's the only thing on the line,
+                 * line is set to null. We could try to continue taking input and ignore
+                 * the ctrl+d press, but exiting seems a bit more standard...
+                 */
+                writeln("Terminating due to end of input.");
+                exit(0);
+            }
+        }
+        catch (std.stdio.StdioException ex)
+        {
+            //Just notify that the exception happened, for now, and just continue on...
+            if(sigintCalled)
+            {
+                //writeln(ex);
+                line.length = 0;
+                sigintCalled = false;
+            }
+            else
+            {
+                //exit because there was an error without the interrupt
+                //ctrl+d will get sent here when you've, in the past, captured a SIGINT
+                //request but continued typing and then later hit ctrl+d.
+                //This is an interesting problem...
+                stderr.writeln(ex);
+                exit(1);
+            }
         }
     }
 }
@@ -60,8 +89,14 @@ void initShell()
             kill(- shell_pgid, SIGTTIN);
         }
 
+        //Setup capture for ctrl+c
+        sigaction_t sigIntHandler;
+        sigIntHandler.sa_handler = &handleControlC;
+        sigemptyset(&sigIntHandler.sa_mask);
+        sigIntHandler.sa_flags = 0;
+
         /* Ignore interactive and job-control signals.  */
-        signal(SIGINT,  SIG_IGN);
+        sigaction(SIGINT, &sigIntHandler, null); //except actually capture ctrl+c
         signal(SIGQUIT, SIG_IGN);
         signal(SIGTSTP, SIG_IGN);
         signal(SIGTTIN, SIG_IGN);
@@ -82,4 +117,12 @@ void initShell()
         /* Save default terminal attributes for shell.  */
         tcgetattr(shell_terminal, &shell_tmodes);
     }
+}
+
+extern(C):
+void handleControlC(int s)
+{
+    writeln("Caught signal ", s);
+    sigintCalled = true;
+    //exit(0);
 }
