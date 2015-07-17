d2f3b4b786efd14b8d83e32d5902a1b72b4cb42d
Devin R <devinr@moback.com>
2015-07-16 17:00:07 -0700

    minor cleanup

--- source/app.d
+++ source/app.d
@@ -34,67 +34,39 @@ void main()
 
 void initShell()
 {
-
     /* See if we are running interactively.  */
     shell_terminal = STDIN_FILENO;
-    shell_is_interactive = isatty (shell_terminal);
+    shell_is_interactive = isatty(shell_terminal);
 
     writeln("Shell is interactive: ", shell_is_interactive);
     if (shell_is_interactive)
     {
         /* Loop until we are in the foreground.  */
         while (tcgetpgrp (shell_terminal) != (shell_pgid = getpgrp()))
-            kill (- shell_pgid, SIGTTIN);
+        {
+            kill(- shell_pgid, SIGTTIN);
+        }
 
         /* Ignore interactive and job-control signals.  */
-        signal (SIGINT, SIG_IGN);
-        signal (SIGQUIT, SIG_IGN);
-        signal (SIGTSTP, SIG_IGN);
-        signal (SIGTTIN, SIG_IGN);
-        signal (SIGTTOU, SIG_IGN);
-        signal (SIGCHLD, SIG_IGN);
+        signal(SIGINT,  SIG_IGN);
+        signal(SIGQUIT, SIG_IGN);
+        signal(SIGTSTP, SIG_IGN);
+        signal(SIGTTIN, SIG_IGN);
+        signal(SIGTTOU, SIG_IGN);
+        signal(SIGCHLD, SIG_IGN);
 
         /* Put ourselves in our own process group.  */
-        shell_pgid = getpid ();
-        if (setpgid (shell_pgid, shell_pgid) < 0)
-          {
-            perror ("Couldn't put the shell in its own process group");
+        shell_pgid = getpid();
+        if (setpgid(shell_pgid, shell_pgid) < 0)
+        {
+            stderr.writeln("Couldn't put the shell in its own process group");
             exit(1);
-          }
+        }
 
         /* Grab control of the terminal.  */
-        tcsetpgrp (shell_terminal, shell_pgid);
+        tcsetpgrp(shell_terminal, shell_pgid);
 
         /* Save default terminal attributes for shell.  */
-        tcgetattr (shell_terminal, &shell_tmodes);
+        tcgetattr(shell_terminal, &shell_tmodes);
     }
 }
-
-/+if (shell_is_interactive)
-  {
-    /* Loop until we are in the foreground.  */
-    while (tcgetpgrp (shell_terminal) != (shell_pgid = getpgrp ()))
-      kill (- shell_pgid, SIGTTIN);
-
-    /* Ignore interactive and job-control signals.  */
-    signal (SIGINT, SIG_IGN);
-    signal (SIGQUIT, SIG_IGN);
-    signal (SIGTSTP, SIG_IGN);
-    signal (SIGTTIN, SIG_IGN);
-    signal (SIGTTOU, SIG_IGN);
-    signal (SIGCHLD, SIG_IGN);
-
-    /* Put ourselves in our own process group.  */
-    shell_pgid = getpid ();
-    if (setpgid (shell_pgid, shell_pgid) < 0)
-      {
-        perror ("Couldn't put the shell in its own process group");
-        exit (1);
-      }
-
-    /* Grab control of the terminal.  */
-    tcsetpgrp (shell_terminal, shell_pgid);
-
-    /* Save default terminal attributes for shell.  */
-    tcgetattr (shell_terminal, &shell_tmodes);
-  }+/
